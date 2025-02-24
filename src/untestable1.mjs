23d5825353924004f624bdccc058de7ea26b8729
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-24 17:42:53 +0200

    Refactor daysUntilChristmas and add test

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,7 +1,18 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
+// "Code which reads the current time (e.g. using new Date()) is inherently untestable."
 export function daysUntilChristmas() {
   const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+  if (today.getTime() > christmasDay.getTime()) {
+    christmasDay.setFullYear(new Date().getFullYear() + 1);
+  }
+  const diffMillis = christmasDay.getTime() - today.getTime();
+  return Math.floor(diffMillis / millisPerDay);
+}
+
+export function daysUntilChristmasRefactored(now) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
